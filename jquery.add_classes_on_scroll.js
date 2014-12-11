8aea7ab297461377542ab77321df96c74af7bbd0
Carlos Cabo <carloscabo@gmail.com>
2014-12-11 17:50:18 +0100

    Update jquery.add_classes_on_scroll.js

--- jquery.add_classes_on_scroll.js
+++ jquery.add_classes_on_scroll.js
@@ -8,7 +8,7 @@ addClassesOnScroll.init([
   // pixels, class to toggle, target object if undefined $('body')
   [100, 'scroll-over-100px'],
   [600, 'scroll-over-500px', $('html')],
-  [$('.main-sub-menu').offset().top, 'scroll-over-element']
+  [$('.main-sub-menu').offset().top, 'class-element-reached']
 ]);
 */
 
